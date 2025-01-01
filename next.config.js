1bbe24b91441141f0084e72ab9744236db92297f
Vito Medlej <vito.medlej@gmail.com>
2025-01-01 21:16:11 +0200

    sitemap fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {};
 
-    async redirects() {
-        return [
-          {
-            source: '/sitemap.xml',
-            destination: '/sitemap.xml/route',
-            permanent: true,
-          },
-        ];
-      },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/api/sitemap.xml/route.ts
+++ src/app/api/sitemap.xml/route.ts
@@ -1,4 +1,3 @@
-// File: `app/sitemap.xml/route.ts`
 import { NextResponse } from 'next/server';
 
 const BASE_URL = 'https://www.earchitecture-lb.com/';
@@ -7,44 +6,43 @@ export async function GET() {
   try {
     // Fetch all product IDs from the get-all-ids API
     const response = await fetch(`${BASE_URL}/api/get-all-ids`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch IDs: ${response.statusText}`);
+    }
     const { ids } = await response.json();
 
     if (!ids || ids.length === 0) {
       throw new Error('No product IDs found.');
     }
 
-    const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-          <loc>${BASE_URL}</loc>
-          <priority>1.0</priority>
-        </url>
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${BASE_URL}</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.earchitecture-lb.com/about</loc>
+    <lastmod>2024-12-05T10:02:12+00:00</lastmod>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://www.earchitecture-lb.com/collection/products</loc>
+    <lastmod>2024-12-05T10:02:12+00:00</lastmod>
+    <priority>0.80</priority>
+  </url>
+  ${ids
+    .map(
+      (id: string) => `
+  <url>
+    <loc>${BASE_URL}/product/${id}</loc>
+    <priority>0.8</priority>
+  </url>`
+    )
+    .join('')}
+</urlset>`;
 
-<url>
-  <loc>https://www.earchitecture-lb.com/about</loc>
-  <lastmod>2024-12-05T10:02:12+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://www.earchitecture-lb.com/collection/products</loc>
-  <lastmod>2024-12-05T10:02:12+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-        ${ids
-          .map(
-            (id: string) => `
-        <url>
-          <loc>${BASE_URL}/product/${id}</loc>
-          <priority>0.8</priority>
-        </url>
-        `
-          )
-          .join('')}
-      </urlset>
-    `;
-
-    return new Response(sitemap, {
+    return new Response(sitemap.trim(), {
       headers: {
         'Content-Type': 'application/xml',
       },
